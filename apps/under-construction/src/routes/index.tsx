59898b57490c752acc32c8fee0f0643660b195ea
Matthias Reis <mr@mreis.me>
2026-03-06 19:07:02 +0100

    feat: added construction site image

--- apps/under-construction/src/routes/index.tsx
+++ apps/under-construction/src/routes/index.tsx
@@ -1,13 +1,29 @@
+import img from '../under-construction.jpg';
+
 export default function Home() {
   return (
-    <main class="flex min-h-screen flex-col items-center justify-center p-4">
-      <div class="text-center space-y-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 class="text-4xl font-bold tracking-tight sm:text-6xl">
-          We're building something new
-        </h1>
-        <p class="text-lg text-muted-foreground max-w-2xl mx-auto">
-          Our platform is currently under construction. Please check back later.
-        </p>
+    <main class="flex min-h-screen flex-col items-center justify-center p-3 font-sans">
+      <div class="grid grid-cols-1 sm:grid-cols-3 gap-5 max-w-2xl">
+        <div class="">
+          <img src={img} alt="Under Construction" class="w-full h-auto" />
+        </div>
+        <div class="col-span-2 text-center">
+          <h1 class="text-3xl sm:text-5xl font-mos mos-effect mb text-cb-0">
+            mosquito.social
+          </h1>
+          <h2 class="text-2xl mb-8">We're building something new</h2>
+          <p class="text-lg text-cf-20 ">
+            Our platform is currently under construction. Please check back
+            later or checkout our development progress at{' '}
+            <a
+              href="https://dev.mosquito.social"
+              class="text-cp-main underline underline-offset-4 hover:text-cs-main"
+            >
+              dev.mosquito.social
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </main>
   );
