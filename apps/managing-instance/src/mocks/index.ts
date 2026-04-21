44a659fb1cc08a6359d5cd875ccee98b2ec744a2
Matthias Reis <mr@mreis.me>
2026-04-21 15:58:46 +0200

    feat: 108 more users

--- apps/managing-instance/src/mocks/index.ts
+++ apps/managing-instance/src/mocks/index.ts
@@ -1,4 +1,5 @@
 export { MOCK_USERS } from './users';
+export { FRANKFURT_USERS } from './frankfurt-users';
 export { MOCK_COMMUNITIES } from './communities';
 export { MOCK_EVENTS } from './events';
 export {
